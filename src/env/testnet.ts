86b81a0f5dd67bd56742d4a3128bdd91b0fe941f
Lucas Isasmendi <lucas.isasmendi@gmail.com>
2019-07-03 21:36:41 +0200

    Use ts-node to start server

--- src/env/testnet.ts
+++ src/env/testnet.ts
@@ -1,4 +1,4 @@
-export const port = +process.env.PORT || 8000;
+export const port = +process.env.PORT || 8080;
 export const jwtSecret = process.env.JWT_SECRET || "your-secret-here";
 export const bnsdTendermintHttpUrl = process.env.BNSD_NODE_HTTP_URL || "https://bns.hugnet.iov.one/";
 export const bnsdTendermintUrl = process.env.BNSD_NODE_URL || "wss://bns.hugnet.iov.one";
