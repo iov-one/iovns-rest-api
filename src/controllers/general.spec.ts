9bdcd308418a3950f310e3b0b9fa1525bb1c58dc
Lucas Isasmendi <lucas.isasmendi@gmail.com>
2019-07-03 15:57:42 +0200

    Add get chain id

--- src/controllers/general.spec.ts
+++ src/controllers/general.spec.ts
@@ -1,12 +1,20 @@
 import * as supertest from "supertest";
 import { app } from "../server";
 
+import { expect } from 'chai';
 const request = supertest.agent(app.listen());
 
 describe("General", () => {
   describe("GET /", () => {
     it("should return welcome message", () => {
       return request.get("/").expect(200, "Welcome to IOV BNS Server!");
+    });
+  });
+  describe("GET /chain", () => {
+    it("should return chain id", () => {
+      return request.get("/chain").expect(200).expect(res => {
+        expect(res.body.chainId).to.be.an('string');
+      });
     });
   });
 });
--- src/controllers/general.ts
+++ src/controllers/general.ts
@@ -1,6 +1,9 @@
 import * as Koa from "koa";
 import * as Router from "koa-router";
 
+import { bnsConnector } from "@iov/bns";
+import { config } from "../config";
+
 const routerOpts: Router.IRouterOptions = {
   prefix: "/"
 };
@@ -9,6 +12,14 @@ const router: Router = new Router(routerOpts);
 
 router.get("/", async (ctx: Koa.Context) => {
   ctx.body = "Welcome to IOV BNS Server!";
+});
+
+router.get("chain", async (ctx: Koa.Context, next: Function) => {
+  await next();
+  const connection = await bnsConnector(config.bnsdTendermintUrl).client();
+  const chainId = connection.chainId();
+  ctx.body = { chainId: chainId };
+  connection.disconnect();
 });
 
 export default router;
