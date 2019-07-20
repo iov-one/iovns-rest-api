d7915b572671a353c7860398864a5de863b01be4
Lucas Isasmendi <lucas.isasmendi@gmail.com>
2019-07-20 13:02:41 +0300

    Update tesnet to kissnet

--- src/env/testnet.ts
+++ src/env/testnet.ts
@@ -1,5 +1,5 @@
 export const port = +process.env.PORT || 8080;
 export const jwtSecret = process.env.JWT_SECRET || "your-secret-here";
-export const bnsdTendermintHttpUrl = process.env.BNSD_NODE_HTTP_URL || "https://bov.hugnet.iov.one/";
-export const bnsdTendermintUrl = process.env.BNSD_NODE_URL || "wss://bov.hugnet.iov.one";
-export const iovFaucet = process.env.IOV_FAUCET || "https://bov-faucet.hugnet.iov.one/";
+export const bnsdTendermintHttpUrl = process.env.BNSD_NODE_HTTP_URL || "https://bns.kissnet.iov.one/";
+export const bnsdTendermintUrl = process.env.BNSD_NODE_URL || "wss://bns.kissnet.iov.one";
+export const iovFaucet = process.env.IOV_FAUCET || "https://bns-faucet.kissnet.iov.one/";
